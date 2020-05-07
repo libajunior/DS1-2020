1e4ad65391b0de9aec727a11b2c8362fd7ec42d9
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-05-07 20:21:34 -0300

    Aula 07/05/2020

---
+++ app-angular/app/controller/cliente.controller.js
@@ -0,0 +1,11 @@
+(function( app ){
+   'use strict';
+   
+   app.controller('ClienteController', function( $scope ){
+
+    //Carrega uma lista de clientes
+    $scope.clientes = ['teste'];
+
+   });
+   
+})( appJS );
