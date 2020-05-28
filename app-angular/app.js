f586c0f5cbf90609a3bc32364106dfd80141061b
Prof. Libório Júnior <prof@liboriojunior.eti.br>
2020-05-28 20:29:31 -0300

    Aula 28/05/2020

--- app-angular/app.js
+++ app-angular/app.js
@@ -1,1 +1,17 @@
-const appJS = angular.module('appJS', ['ngStorage']);
+const appJS = angular.module('appJS', ['ngStorage', 'ngRoute']);
+
+//Configuração das rotas
+appJS.config(function($routeProvider){
+
+    $routeProvider
+        .when('/clientes', {
+            templateUrl: 'app/view/cliente.view.html',
+            controller: 'ClienteController'
+        })
+        .when('/estados', {
+            templateUrl: 'app/view/estado.view.html',
+            controller: 'EstadoController'
+        })
+        .otherwise({redirectTo: '/clientes'});
+
+});
